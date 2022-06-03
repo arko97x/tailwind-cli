55f741ae71f29d8e1b1649ebf63cadbd74005d9c
Arko <bhattacharjee.arkoprabho@gmail.com>
2022-06-03 20:51:48 +0530

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./layouts/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        'lblue': '#0000ff',
+        'lbg1': '#c1cfca',
+        'lbg1hover': '#9dbbb0',
+        'lbg2': '#e7e0c9',
+      },
+    },
+  },
+  plugins: [],
+}
